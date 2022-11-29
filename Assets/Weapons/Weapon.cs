f5b0dbc6556e839d96afc4ac59a3a348ec2fa1ca
July Rocha <nrochajuly@gmail.com>
2022-11-29 16:39:23 -0300

    :sparkles: Make enemy health and gun damage system

--- Assets/Weapons/Weapon.cs
+++ Assets/Weapons/Weapon.cs
@@ -7,6 +7,7 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30;
 
     void Update()
     {
@@ -19,7 +20,20 @@ public class Weapon : MonoBehaviour
     void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        Debug.Log($"I hit: {hit.transform.name}");
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log($"I hit: {hit.transform.name}");
+            //TODO: Add VFX for player 
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null)
+            {
+                return;
+            }
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
     }
 }
