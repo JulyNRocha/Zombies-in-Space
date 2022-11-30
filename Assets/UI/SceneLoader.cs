b65f136219a7ae6baefe6c1e00537f814956d0e3
July Rocha <nrochajuly@gmail.com>
2022-11-30 20:32:14 -0300

    :lipstick: Create UI screen

---
+++ Assets/UI/SceneLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
