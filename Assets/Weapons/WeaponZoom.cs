dd54826519c7ec4adad22519e5d65de76e4947c3
July Rocha <nrochajuly@gmail.com>
2022-12-01 18:54:05 -0300

    :sparkles: Make a zoom for the weapon

---
+++ Assets/Weapons/WeaponZoom.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutSensitivity = 3f;
+    [SerializeField] float zoomedInSensitivity = 0.5f;
+     
+    RigidbodyFirstPersonController fpsController;
+
+    bool zoomedInToggle = false;
+
+    void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                fpsCamera.fieldOfView = zoomedInFOV;
+                fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
+            }
+            else
+            {
+                zoomedInToggle = false;
+                fpsCamera.fieldOfView = zoomedOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
+            }
+        }
+    }
+
+}
