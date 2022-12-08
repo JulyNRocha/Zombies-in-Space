6dd6acba1bc2f83e771a95e5277bb6ccf777635b
July Rocha <nrochajuly@gmail.com>
2022-12-08 16:01:48 -0300

    :sparkles: Add zombie asset ans anim

--- Assets/Enemy/Scripts/EnemyHealth.cs
+++ Assets/Enemy/Scripts/EnemyHealth.cs
@@ -6,13 +6,23 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] float hitPoint = 100f;
 
+    bool isDead = false;
+    public bool IsDead { get { return isDead;} }
+
     public void TakeDamage(float damage)
     {
         BroadcastMessage("OnDamageTaken");
         hitPoint -= damage;
         if(hitPoint <= 0)
         {
-            Destroy(this.gameObject);
+            Die();
         }
+    }
+
+    void Die()
+    {
+        if ( isDead ) return;
+        isDead = true;
+        GetComponent<Animator>().SetTrigger("die");
     }
 }
